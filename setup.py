9c591129d7fb0728415bce3d38a9f38a4cb263a5
Bryce Hemme <bryce.hemme@pioneer.com>
2018-02-25 12:08:45 -0600

    move files

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='Cirro',
+    version='0.1dev',
+    packages=['cirro',],
+    license='MIT License',
+    long_description=open('README.txt').read(),
+)
