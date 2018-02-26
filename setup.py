893aa0ad84a98d7a5fa3f61c75e7bc821247b080
Bryce Hemme <bryce.hemme@gmail.com>
2018-02-25 21:33:50 -0600

    flesh out commands

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from distutils.core import setup
+from pip.req import parse_requirements
+
+install_reqs = parse_requirements('requirements.txt')
+reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
     name='Cirro',
     version='0.1dev',
     packages=['cirro',],
     license='MIT License',
-    long_description=open('README.txt').read(),
+    long_description=open('README.md').read(),
+    install_requires=reqs
 )
